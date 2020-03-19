dcd888063feb5b48042c4befa3898ca1eaa10744
Ching Chen <stone2809@gmail.com>
2020-03-19 23:14:45 +0800

    My 2nd Version

--- pw.py
+++ pw.py
@@ -1,14 +1,17 @@
 password = 'a123456'
 pw_num = 3
-while True:
+while pw_num > 0:
+	pw_num = pw_num - 1 
 	pw = input('Enter your password: ')
 	if pw == password:
 		print('Login Successfully')
 		break
 	else:
-		pw_num = pw_num - 1
-		print('Wrong Password, ', 'You have ', pw_num ,'time(s) left' )
 		if pw_num == 0:
 			print('Password Incorrect')
 			print('Program Shut Down')
-			break
+		else:
+			print('Wrong Password, ')
+			print('You have ', pw_num, 'time(s) left')
+		
+			
